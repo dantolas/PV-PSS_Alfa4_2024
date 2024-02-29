4d7808ae0c3dc03b708e1bd34fad1fe01f4b1bac
Prince Charming <kuta.samuel@gmail.com>
2024-02-29 11:27:45 +0100

    Frontend

--- src/main/resources/public/scripts/script.js
+++ src/main/resources/public/scripts/script.js
@@ -4,6 +4,8 @@ document.querySelector(".refresh").addEventListener("click",()=>{
     console.log("xd");
 })
 
-$(".refresh").addEventListener("click",()=>{
-    console.log("Overriden");
-})
+$(document).ready(function(){
+    the_h1_text = $("h1#title").text();
+    alert(the_h1_text);
+    console.log(the_h1_text);
+});
